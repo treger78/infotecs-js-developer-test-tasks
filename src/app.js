85285a45b1439c270520ce0ee5c8eed4c8074ff5
Ilia <Undefined>
2025-07-18 20:20:24 +0300

    feat: implemented 1-3 points of task1

--- src/app.js
+++ src/app.js
@@ -1,16 +1,46 @@
-const initCollage = async () => {
-    const requestImages = await fetch('https://dog.ceo/api/breeds/image/random/3');
-    const response = await requestImages.json();
+const COLLAGE = document.getElementById('collage');
+const selectLimit = document.getElementById('imgLimit');
 
-    const collage = document.getElementById('collage');
+const getImgLimitValue = () => selectLimit.value;
 
-    response.message.forEach(image => {
+const imgCounter = () => document.querySelectorAll('img').length;
+
+const requestImages = async (imgLimitNumber) => {
+    return (await fetch(`https://dog.ceo/api/breeds/image/random/${imgLimitNumber}`)).json();
+};
+
+const createAndAppendNewImg = (imgArray) => {
+    imgArray.forEach(image => {
         const img = document.createElement('img');
 
         img.src = image;
 
-        collage.append(img);
+        COLLAGE.append(img);
     });
+};
+
+const updateCollage = async (mode = 'update') => {
+    if (mode === 'changeLimit') COLLAGE.replaceChildren();
+
+    const response = await requestImages(1);
+
+    if (imgCounter() >= getImgLimitValue()) COLLAGE.firstChild.remove();
+
+    createAndAppendNewImg(response.message);
+};
+
+const imgLimitHandler = () => selectLimit.addEventListener('change', () => updateCollage(mode = 'changeLimit'));
+
+const initCollage = async () => {
+    const response = await requestImages(getImgLimitValue());
+
+    createAndAppendNewImg(response.message);
+
+    imgLimitHandler();
+
+    setInterval(() => {
+        updateCollage();
+    }, 3000);
 };
 
 initCollage();
