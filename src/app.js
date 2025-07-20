d03b4485c785cc1bbfb5827a3878f0ca46d2e126
Ilia <Undefined>
2025-07-20 15:25:20 +0300

    feat: implement point 2 of task 2

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const getAndTransformUsers = async () => {
+const getUsers = async () => {
     try {
         const response = await fetch('https://dummyjson.com/users?limit=0');
 
@@ -6,72 +6,77 @@ const getAndTransformUsers = async () => {
 
         const { users } = await response.json();
 
-        const formatAddress = (obj) => {
-            if (!obj) return '';
-
-            return [
-                obj.address,
-                obj.city,
-                obj.country,
-                obj.postalCode,
-                obj.state,
-                obj.stateCode,
-            ].filter(Boolean).join(', ');
-        };
-
-        const formatCoordinates = (coordinates) => {
-            return coordinates?.lat && coordinates?.lng ? `${coordinates.lat}, ${coordinates.lng}` : undefined;
-        };
-
-        return users.map((user) => {
-            return {
-                id: user.id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                maidenName: user.maidenName,
-                age: user.age,
-                gender: user.gender,
-                email: user.email,
-                phone: user.phone,
-                username: user.username,
-                password: user.password,
-                birthDate: user.birthDate,
-                image: user.image,
-                bloodGroup: user.bloodGroup,
-                height: user.height,
-                weight: user.weight,
-                eyeColor: user.eyeColor,
-                hairColor: user.hair.color,
-                hairType: user.hair.type,
-                university: user.university,
-                userAddress: formatAddress(user.address),
-                userCoordinates: formatCoordinates(user.address?.coordinates),
-                macAddress: user.macAddress,
-                ip: user.ip,
-                role: user.role,
-                ein: user.ein,
-                ssn: user.ssn,
-                userAgent: user.userAgent,
-                companyName: user.company?.name,
-                companyDepartment: user.company?.department,
-                companyTitle: user.company?.title,
-                companyAddress: formatAddress(user.company?.address),
-                companyCoordinates: formatCoordinates(user.company?.address?.coordinates),
-                bankCardNumber: user.bank?.cardNumber,
-                bankCardExpire: user.bank?.cardExpire,
-                bankCardType: user.bank?.cardType,
-                bankCurrency: user.bank?.currency,
-                bankIban: user.bank?.iban,
-                cryptoCoin: user.crypto?.coin,
-                cryptoWallet: user.crypto?.wallet,
-                cryptoNetwork: user.crypto?.network,
-            }
-        });
+        return users;
     } catch (error) {
-        console.error('Error in getAndTransformUsers: ', error);
+        console.error('Error fetching users: ', error);
 
         return [];
     }
+};
+
+//TODO: подумать стоит ли действительно преобразовывать данные
+const transformUsers = (users) => {
+    const formatAddress = (obj) => {
+        if (!obj) return '';
+
+        return [
+            obj.address,
+            obj.city,
+            obj.country,
+            obj.postalCode,
+            obj.state,
+            obj.stateCode,
+        ].filter(Boolean).join(', ');
+    };
+
+    const formatCoordinates = (coordinates) => {
+        return coordinates?.lat && coordinates?.lng ? `${coordinates.lat}, ${coordinates.lng}` : undefined;
+    };
+
+    return users.map((user) => {
+        return {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            maidenName: user.maidenName,
+            age: user.age,
+            gender: user.gender,
+            email: user.email,
+            phone: user.phone,
+            username: user.username,
+            password: user.password,
+            birthDate: user.birthDate,
+            image: user.image,
+            bloodGroup: user.bloodGroup,
+            height: user.height,
+            weight: user.weight,
+            eyeColor: user.eyeColor,
+            hairColor: user.hair.color,
+            hairType: user.hair.type,
+            university: user.university,
+            userAddress: formatAddress(user.address),
+            userCoordinates: formatCoordinates(user.address?.coordinates),
+            macAddress: user.macAddress,
+            ip: user.ip,
+            role: user.role,
+            ein: user.ein,
+            ssn: user.ssn,
+            userAgent: user.userAgent,
+            companyName: user.company?.name,
+            companyDepartment: user.company?.department,
+            companyTitle: user.company?.title,
+            companyAddress: formatAddress(user.company?.address),
+            companyCoordinates: formatCoordinates(user.company?.address?.coordinates),
+            bankCardNumber: user.bank?.cardNumber,
+            bankCardExpire: user.bank?.cardExpire,
+            bankCardType: user.bank?.cardType,
+            bankCurrency: user.bank?.currency,
+            bankIban: user.bank?.iban,
+            cryptoCoin: user.crypto?.coin,
+            cryptoWallet: user.crypto?.wallet,
+            cryptoNetwork: user.crypto?.network,
+        }
+    });
 };
 
 const getPostsByUserID = async (userID) => {
@@ -88,12 +93,47 @@ const getPostsByUserID = async (userID) => {
     }
 };
 
-//TODOs: разбить getAndTransformUsers на 2 функции;
+const renderTheadData = (thDataArray) => {
+    const theadRow = document.getElementById('users-table-thead-row');
+    const fragment = document.createDocumentFragment();
+
+    thDataArray.forEach(element => {
+        const th = document.createElement('th');
+
+        th.textContent = element;
+
+        fragment.appendChild(th);
+    });
+
+    theadRow.appendChild(fragment);
+};
+
+const renderTbodyData = (users) => {
+    const tbody = document.getElementById('users-table-tbody');
+    const fragment = document.createDocumentFragment();
+
+    users.forEach(user => {
+        const tr = document.createElement('tr');
+
+        Object.values(user).forEach(value => {
+            const td = document.createElement('td');
+
+            td.textContent = value;
+
+            tr.appendChild(td);
+        });
+
+        fragment.appendChild(tr);
+    });
+
+    tbody.appendChild(fragment);
+};
 
 const initUsersTable = async () => {
-    const users = await getAndTransformUsers();
+    const users = transformUsers(await getUsers());
 
-    console.log(users);
+    renderTheadData(Object.keys(users[0]));
+    renderTbodyData(users);
 
     //const posts = await getPostsByUserID(2);
     //console.log(posts);
